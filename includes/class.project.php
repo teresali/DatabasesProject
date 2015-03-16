ad82d932b8a6e00cd32ff1d8a592eacee7f7fc78
Teresa Li <tml87@cornell.edu>
2015-03-16 11:14:08 +0000

    fixed formatting

--- includes/class.project.php
+++ includes/class.project.php
@@ -57,7 +57,6 @@ class Project {
     return $scores;
   }
     
-
   public function getRankForUser($db, $projectId, $groupId) {
     $q = "SELECT groupAssessed, avgScore, @curRank := @curRank + 1 as rank 
           FROM (
@@ -75,29 +74,21 @@ class Project {
     return NULL;
   }
    
-    public function isDuplicate($email, $db) {
-        $check_dup = $db->query("SELECT * FROM projects WHERE projectTitle = '{$title}'");
-        if ($check_dup->num_rows == 1) {
-          return True;
-        }
-        return False;
+  public function isDuplicate($email, $db) {
+    $check_dup = $db->query("SELECT * FROM projects WHERE projectTitle = '{$title}'");
+    if ($check_dup->num_rows == 1) {
+      return True;
     }
+    return False;
+  }
     
-    public function addProject($data, $db) {
+  public function addProject($data, $db) {
     // add project info to database
     $q = "INSERT INTO projects (projectTitle, dueDate, projectDescription, criteria1, criteria2, criteria3, criteria4, criteria5) VALUES ('{$data['projectTitle']}', '{$data['dueDate']}', '{$data['projectDescription']}', {$data['criteria1']}, {$data['criteria2']}, {$data['criteria3']}, {$data['criteria4']}, {$data['criteria5']})";
         echo $q;
     $db->query($q);
-    }
+  }
   
-    public function addProject($data, $db) {
-    // add project info to database
-    $q = "INSERT INTO projects (projectTitle, dueDate, projectDescription, criteria1, criteria2, criteria3, criteria4, criteria5) VALUES ('{$data['projectTitle']}', '{$data['dueDate']}', '{$data['projectDescription']}', {$data['criteria1']}, {$data['criteria2']}, {$data['criteria3']}, {$data['criteria4']}, {$data['criteria5']})";
-        echo $q;
-    $db->query($q);
-    }
-    
-    
 }
 
 
